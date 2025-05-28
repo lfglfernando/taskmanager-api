64eca33e8ef5cfb0849923abef130e79894fbe1e
lfglfernando <lfglfernandog@gmail.com>
2025-05-27 20:20:20 -0600

    Fix swagger

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+
+  
 const { connectToServer } = require('./db/conn');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
+
+const swaggerDocs = require('./swagger');
+swaggerDocs(app);
 
 const port = process.env.PORT || 3000;
 
@@ -16,5 +28,3 @@ connectToServer(() => {
   });
 });
 
-const swaggerDocs = require('./swagger');
-swaggerDocs(app);
